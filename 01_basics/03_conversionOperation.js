f81d594260d25a1fd786cd05903dd1a265780e36
Prachi-Budhiraja <prachibudhiraja0@gmail.com>
2025-07-08 12:25:05 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33abc"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber )
+console.log(valueInNumber)
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1 or false => 0
+
+
+let loggedIn = 1
+let booleanIsLoggedIn = Boolean(loggedIn)
+
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+// 1 => true , 0 => false
+// "" => false
+// "Prachi" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)

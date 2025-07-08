ea169fa11c661ae69fdd819122171b71e49529df
Prachi-Budhiraja <prachibudhiraja0@gmail.com>
2025-07-08 11:56:00 +0000

    datatypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict";  // treat all JS code as newest version
+
+alert(3+3)  // we qare using nodeJS not browser
+
+console.log(3
+    +3)   // readability should be high
+
+console.log("Prachi")
+
+let name = "Prachi"
+let age = 21
+let isLoggedIn = false
+
+
+
+// number => 2 to the power 53
+// bigint
+//string => ""
+// boolean =>true/fale
+// null => standard alone
+// undefined ==> 
+// symbol => unique
+
+// object 
+
+
+console.log(typeof undefined)  // undefined
+console.log(typeof null)   // object

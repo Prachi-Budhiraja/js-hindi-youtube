cc47079cc1cf5b8112e3b5534b75aaa3f833b5fe
Prachi-Budhiraja <prachibudhiraja0@gmail.com>
2025-07-08 11:49:03 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountID = 14337
+let accountEmail = "prachi@google.com"
+var accountPassword = "12345"
+accountName = "Prachi"
+let accountState;
+
+
+// accountID = 123434  // not allowed
+accountEmail = "gdhws.google.com"
+accountPassword= "15432"
+accountName = "Budhiraja"
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+
+*/
+
+
+
+
+console.log(accountID)
+
+console.table([accountID, accountEmail, accountPassword, accountName, accountState])

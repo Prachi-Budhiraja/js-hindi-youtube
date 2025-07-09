81f33a0b290c2ecc372a9d0d495aa7746b83253a
Prachi-Budhiraja <prachibudhiraja0@gmail.com>
2025-07-09 09:01:24 +0000

    comparison

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,3 +28,46 @@ let stringNumber = String(someNumber)
 
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+
+
+// ****************************** Operations ******************************************
+
+
+let value = 3
+let negvalue = -value
+console.log(negvalue);
+
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2);
+console.log(2**2);   // 2 to the power 2
+
+
+
+let str1 = "Hello"
+let str2 = "Prachi"
+
+let str3 = str1 + " " + str2
+console.log(str3);
+ 
+console.log("1" + 2)  // result = 12
+console.log(1 + "2"); // result = 12
+console.log("1" +2 +2);  // result = 122
+console.log(1+ 2+ "2")  //result = 32
+
+
+console.log(+true)  // result =1
+console.log(+"");  // result = 0
+
+
+let gameCounter = 100
+gameCounter++;  // result = 101
+++gameCounter // result = 101
+
+// to learn about thid go on mdn pe increment and post increment
+
+
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,28 @@
+console.log(2 >= 1);
+console.log(2 <1);
+console.log(4 <= 5);
+
+console.log("2" > 1); // result = true
+console.log("02" > 1); // result = true
+
+
+console.log(null > 0);  // false
+console.log(null == 0 ); // false
+console.log(null >= 0);  // true
+
+console.log(undefined>0);  // false
+console.log(undefined == 0 );  // false
+console.log(undefined >= 0 );  // false
+
+
+// "==="  this will check data type also
+
+console.log("2" === 2);  // false
+
+ 
+
+
+
+
+
+
